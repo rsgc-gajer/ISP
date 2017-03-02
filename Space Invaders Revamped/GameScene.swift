e62079e6737012765d2b78fd363f18bc7c1c8c05
Ryan Gajer <rgajer@MacBook-Pro-RGajer.local>
2017-03-02 07:56:20 -0500

    Added images to my game. I also added code so the player and the background appear on the screen. I was working on firing a bullet and I would like to have that done soon.

--- Space Invaders Revamped/GameScene.swift
+++ Space Invaders Revamped/GameScene.swift
@@ -1,89 +1,47 @@
-//
-//  GameScene.swift
-//  Space Invaders Revamped
-//
-//  Created by Ryan Gajer on 2017-02-28.
-//  Copyright © 2017 Ryan Gajer. All rights reserved.
-//
+// GameScence.Swift
+
+// Space Invaders
+
+// Created by Ryan Gajer
 
 import SpriteKit
-import GameplayKit
 
-class GameScene: SKScene {
+class GameScene : SKScene {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+     let player = SKSpriteNode(imageNamed: "ship") // Import playermodel image
+    
+    // This function runs once at the start of the game
     
     override func didMove(to view: SKView) {
+        // Background
+        let background = SKSpriteNode(imageNamed: "stars") // Import the background image
+        background.size = self.size
+        background.position = CGPoint(x: self.size.width/2, y: self.size.height/2)
+        background.zPosition = 0 // Lowest zPosition meaning it will be behind everything
+        self.addChild(background) // Add background to the scene
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
-        
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
+        // Player
+        player.setScale(0.9) // Set player scale size
+        player.position = CGPoint(x: self.size.width/2, y: self.size.height/2 * 0.2) // Middle of the screen, up about 1/4 on screen
+        self.zPosition = 2 // Will be above the background
+        self.addChild(player)
+    }
+    
+        // Bullet
+        func bulletFired() {
+            let bullet = SKSpriteNode(imageNamed: "bullet") // Import the bullet image
+            bullet.setScale(1)
+            bullet.position = player.position // Setting the bullet so it stays with the player model
+            bullet.zPosition = 1 // Behind playermodel but in front of background
+            self.addChild(bullet)
             
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(M_PI), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
+        // Implement bullet actions
+            let bulletMovement = SKAction.moveTo(y: self.size.height + bullet.size.height, duration: 1) // Move bullet along y axis only with the height of the window
+            let bulletDelete = SKAction.removeFromParent()
+            let bulletSequence = SKAction.sequence([bulletMovement,bulletDelete]) // Runs animation of bullet appearing and then deleting after duration
+            bullet.run(bulletSequence) // Run the sequence
         }
-    }
-    
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
+        // Run bulletFired()
         
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
     }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
-    }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    
-    override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
-    }
-}
+//}
--- Space Invaders Revamped/GameViewController.swift
+++ Space Invaders Revamped/GameViewController.swift
@@ -15,21 +15,20 @@ class GameViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        if let view = self.view as! SKView? {
-            // Load the SKScene from 'GameScene.sks'
-            if let scene = SKScene(fileNamed: "GameScene") {
+        let scene = GameScene(size: CGSize(width: 1536, height: 2048))
+                // Configure the view
+                let skView = self.view as! SKView
+                skView.showsFPS = true
+                skView.showsNodeCount = true
+        
+                // Improve rednering performance
+                skView.ignoresSiblingOrder = true
+        
                 // Set the scale mode to scale to fit the window
                 scene.scaleMode = .aspectFill
                 
                 // Present the scene
-                view.presentScene(scene)
-            }
-            
-            view.ignoresSiblingOrder = true
-            
-            view.showsFPS = true
-            view.showsNodeCount = true
-        }
+                skView.presentScene(scene)
     }
 
     override var shouldAutorotate: Bool {
